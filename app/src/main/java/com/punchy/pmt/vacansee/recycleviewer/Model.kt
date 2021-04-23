683ac5db45a5b6836c1776bfeb71e63a1ef93d20
Alexander Petrov <alexander.petrov420@gmail.com>
2021-04-23 22:13:22 +0100

    recycler added

---
+++ app/src/main/java/com/punchy/pmt/vacansee/recycleviewer/Model.kt
@@ -0,0 +1,3 @@
+package com.punchy.pmt.vacansee.recycleviewer
+
+data class Model(val name: String = "", val count: Int = 0)
--- app/src/main/java/com/punchy/pmt/vacansee/recycleviewer/RVactivity.kt
+++ app/src/main/java/com/punchy/pmt/vacansee/recycleviewer/RVactivity.kt
@@ -1,56 +1,34 @@
-package com.punchy.pmt.vacansee
+package com.punchy.pmt.vacansee.recycleviewer
+
 
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.ColorSpace
 import android.graphics.Rect
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.punchy.pmt.vacansee.recycleviewer.RvAdapter
+import com.punchy.pmt.vacansee.R
+import kotlin.math.roundToInt
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [JobsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class JobsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
         val trashBinIcon = resources.getDrawable(
             R.drawable.ic_baseline_archive_24,
             null
         )
-
-        val jobsView: View = inflater.inflate(R.layout.activity_jobs_list, container, false)
-        // Inflate the layout for this fragment
-        val recyclerView = jobsView.findViewById<RecyclerView>(R.id.recyclerView)
+        super.onCreate(savedInstanceState)
+//        Set layout file to class
+        setContentView(R.layout.activity_main)
+//        initialize the recyclerView from the XML
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 //        Initializing the type of layout, here I have used LinearLayoutManager you can try GridLayoutManager
 //        Based on your requirement to allow vertical or horizontal scroll , you can change it in  LinearLayout.VERTICAL
-        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 //        Create an arraylist
         val dataList = ArrayList<String>()
         dataList.add("aasdas")
@@ -112,27 +90,9 @@ class JobsFragment : Fragment() {
         myHelper.attachToRecyclerView(recyclerView)
 
 
-
-        return jobsView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment JobsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            JobsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
 }
