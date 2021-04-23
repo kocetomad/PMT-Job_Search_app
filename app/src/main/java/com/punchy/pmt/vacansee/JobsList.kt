fbe404430a176cea45a02f9fca0e0cc5514df1c1
Stefan Pruneanu <stefanpruneanu@gmail.com>
2021-04-23 17:04:37 +0300

    added basic request code

--- app/src/main/java/com/punchy/pmt/vacansee/JobsList.kt
+++ app/src/main/java/com/punchy/pmt/vacansee/JobsList.kt
@@ -2,15 +2,49 @@ package com.punchy.pmt.vacansee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.LinearLayout
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+import okhttp3.Response
 
 class JobsList : AppCompatActivity() {
+    private val client = OkHttpClient()
+
+    private fun getReedJobs() {
+        val request = Request.Builder()
+            .url("https://www.reed.co.uk/api/1.0/search?locationName=bournemouth")
+            .addHeader("Authorization", "Basic ODAxMDc2ZGEtYTViYi00YjhlLWE5YTUtZDVkNDJiMDQ3OWYwOg==")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) {
+                        throw IOException("Unexpected code $response")
+                    }
+                    /*for ((name, value) in response.headers) {
+                        Log.d("JobsList - headers","$name: $value")
+                    }*/
+
+                    Log.d("JobsList - body", response.body!!.string())
+                }
+            }
+        })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jobs_list)
 
+        // is null safe because the activity may not have an action bar defined.
         supportActionBar?.elevation = 0F // to remove the drop shadow from the action bar
 
         // val backdropView = findViewById<LinearLayout>(R.id.backdropView)
@@ -21,5 +55,10 @@ class JobsList : AppCompatActivity() {
         or backdropSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED */
 
         // TODO add filters for job queries
+
+        val getJobsButton = findViewById<Button>(R.id.getJobsButton)
+        getJobsButton.setOnClickListener {
+            getReedJobs()
+        }
     }
 }
