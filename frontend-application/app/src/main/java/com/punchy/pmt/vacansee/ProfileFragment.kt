02b43350fbe5905b2d1542dcd143249040e537c4
Stefan Pruneanu <stefanpruneanu@gmail.com>
2021-04-29 19:24:34 +0300

    fix profile page

--- frontend-application/app/src/main/java/com/punchy/pmt/vacansee/ProfileFragment.kt
+++ frontend-application/app/src/main/java/com/punchy/pmt/vacansee/ProfileFragment.kt
@@ -6,7 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.punchy.pmt.vacansee.searchJobs.httpRequests.getSavedJobs
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +28,13 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // remove action bar shadow
+        (activity as AppCompatActivity?)!!.supportActionBar?.elevation = 0f
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +53,24 @@ class ProfileFragment : Fragment() {
 
         profileView.findViewById<Button>(R.id.goToJobsSearch).setOnClickListener {
             findNavController().navigate(R.id.action_profileFragment_to_jobsFragment)
+        }
+
+        val bottomSheetView = profileView.findViewById<LinearLayout>(R.id.savedJobsBackdrop)
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView)
+
+        val savedJobsList = getSavedJobs()
+
+        if (savedJobsList.isEmpty()) {
+            // get progress bar and hide it after the jobs load.
+            bottomSheetView.findViewById<ProgressBar>(R.id.savedJobsProgressBar).visibility = View.GONE
+
+            // get error view and make it visible if the fetching fails
+            bottomSheetView.findViewById<TextView>(R.id.savedJobsErrorText).text =
+                "No saved jobs found"
+            bottomSheetView.findViewById<LinearLayout>(R.id.savedJobsErrorView).visibility = View.VISIBLE
+        } else {
+            // get progress bar and hide it after the jobs load.
+            bottomSheetView.findViewById<ProgressBar>(R.id.savedJobsProgressBar).visibility = View.GONE
         }
 
         return profileView
